50cac208e26f5c2df2aa72117d44e150ffa8a6eb
Lorcan <lorcan.james@yahoo.com>
2021-02-07 10:52:20 +0000

    add Distances Wrapper class

--- Distances.py
+++ Distances.py
@@ -6,4 +6,11 @@ class Distances:
         self.cells = {}
         self.cells[self.root] = 0
 
-    def
+    def return_distance(self, cell):
+        return self.cells[cell]
+    
+    def record_distance(self, cell, distance):
+        self.cells[cell] = distance
+    
+    def return_cells(self):
+        return self.cells.keys
